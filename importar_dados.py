1773712142ff0354b768e49f0bc5ec2c2cd04557
Ângela Oliveira <89156781+angela-oliveir@users.noreply.github.com>
2022-04-21 19:23:21 -0300

    Atualizei o script para ler cada um dos clientes e seus respectivos dados
    Depois de ler e analisar os dados, percebi que tinham clientes que estava com o campo "nome" vazio, por esse motivo adicionei uma condição para ler apenas os dados esteja com esse campo preenchido.

--- importar_dados.py
+++ importar_dados.py
@@ -1,7 +1,33 @@
+
 import os
+from time import sleep
+import pandas as pd
+import csv
+
+print('Listando dados de clientes:') 
+print('')
+sleep(2)
 pasta = './arquivos-para-analise'
 for diretorio, subpastas, arquivos in os.walk(pasta):
     for arquivo in sorted(arquivos):
-        if 'clients' in arquivo:
+        if 'clients-' not in arquivo:
+            continue
 
-            print(arquivo)
+        caminho = f'{pasta}/{arquivo}'
+        df = pd.read_csv(
+            caminho, 
+            encoding= 'LATIN-1', 
+            sep= ';', 
+            )
+        for index, row in df.iterrows():
+            if 'id' in row:
+                print(f'id = {row.id}')
+            if 'nome' in row:
+                print(f'nome = {row.nome}')
+            if 'email' in row:
+                print(f'email = {row.email}')
+            if 'data_cadastro' in row:
+                print(f'data_cadastro = {row.data_cadastro}')   
+            if 'telefone' in row:
+                print(f'telefone = {row.telefone}')         
+                print('')
