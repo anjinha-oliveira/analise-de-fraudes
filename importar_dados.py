83b030c6ead322fe19f472bf7041c9ee1226f164
Ângela Oliveira <89156781+angela-oliveir@users.noreply.github.com>
2022-04-27 21:16:16 -0300

    Inseri clientes no banco de dados

--- importar_dados.py
+++ importar_dados.py
@@ -3,6 +3,14 @@ import os
 from time import sleep
 import pandas as pd
 
+import email
+import os
+from sqlite3 import Cursor
+from tkinter import INSERT
+
+from numpy import insert
+import pyodbc
+
 print('Listando dados de clientes:\n') 
 sleep(2)
 pasta = './arquivos-para-analise'
@@ -13,15 +21,39 @@ for diretorio, subpastas, arquivos in os.walk(pasta):
 
         caminho = f'{pasta}/{arquivo}'
         df = pd.read_csv(caminho, encoding= 'LATIN-1', sep= ';')
+        
         for index, row in df.iterrows():
-            if 'id' in row:
-                print(f'id = {row.id}')
-            if 'nome' in row:
-                print(f'nome = {row.nome}')
-            if 'email' in row:
-                print(f'email = {row.email}')
-            if 'data_cadastro' in row:
-                print(f'data_cadastro = {row.data_cadastro}')   
-            if 'telefone' in row:
-                print(f'telefone = {row.telefone}')         
-                print('')
+            # if [0] in row:
+            #     print(row[0])
+            #     print(f'id = {row[0]}')
+            print(f'id = {row[0]}')
+            print(f'nome = {row[1]}')
+            print(f'email = {row[2]}')
+            print(f'data_cadastro = {row[3]}')
+            print(f'telefone = {row[4]}')        
+            print('')
+
+            dados_de_conexao = (
+                "Driver={SQL Server};"
+                "Server=BRENO-LAPTOP;"
+                "Database=Fraudes;"
+            )
+
+            conexao = pyodbc.connect(dados_de_conexao)
+            
+            cursor = conexao.cursor()
+            print('Conexão bem sucedida')
+
+            id_on = "SET IDENTITY_INSERT clientes ON"
+            cursor.execute(id_on)
+            print('Conexão id on sucedida')
+
+            sqlserver = f"""INSERT INTO clientes(id, nome, email, data_cadastro, telefone) 
+                        VALUES('{row[0]}',  '{row[1]}', '{row[2]}', '{row[3].replace(" -0300", "")}', '{row[4]}')"""
+            
+            
+
+            #PARA EXECUTAR O COMANDO SQL
+            print(sqlserver)
+            cursor.execute(sqlserver)
+            cursor.commit()
