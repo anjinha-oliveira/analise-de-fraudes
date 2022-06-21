dddac37808a8b11138a4327af752b7231123d8cf
Ângela Oliveira <89156781+angela-oliveir@users.noreply.github.com>
2022-06-21 20:20:43 -0300

    Atualizei o script para a consultar e ignorar id de transações e também id de clientes que não existem na tabela clientes, fiz uma nova conexão com o banco para
    Atualizei o script para a consultar e ignorar id de transações e também id de clientes que não existem. Fiz uma nova conexão com o banco para carregar esses dados dentro da tabela transacoes no banco

--- importar_dados.py
+++ importar_dados.py
@@ -52,6 +52,7 @@ for diretorio, subpastas, arquivos in os.walk(pasta):
                 cursor.execute(clientes)
                 cursor.commit()
                 print(f'cliente id = {row[0]}, inserido com sucesso')
+
 print('\n')
 print('Listando arquivos de transações:\n') 
 sleep(2)
@@ -62,9 +63,44 @@ for diretorio, subpastas, arquivos in os.walk(pasta):
             caminho_transacoes = f'{pasta}/{arquivo}'
             df = pd.read_csv(caminho_transacoes, encoding= 'LATIN-1', sep= ';')                    
             for index, row in df.iterrows():
+                consulta_id = f"select 1 from clientes where id = '{row[1]}'"
+                cursor.execute(consulta_id)
+                resultado = cursor.fetchone()
+
+                if resultado is None:
+                    print(f'Ignorando id de transacoes {row[0]}')
+                    continue
+                
+                consulta_transacao = f"select 1 from transacoes where id = '{row[0]}'"
+                cursor.execute(consulta_transacao)
+                resultado = cursor.fetchone()
+                if resultado is not None:
+                    print(f'Ignorando transacoes {row[0]}')
+                    continue
+                
+
                 print(f'id = {row[0]}')
                 print(f'cliente_id = {row[1]}')
                 print(f'valor = {row[2]}')
                 print(f'data = {row[3]}')      
                 print()
-                
+
+                dados_de_conexao = (
+                    "Driver={SQL Server};"
+                    "Server=BRENO-LAPTOP;"
+                    "Database=Fraudes;"
+                )
+
+                conexao = pyodbc.connect(dados_de_conexao)
+                            
+                cursor = conexao.cursor()
+
+                id_on = "SET IDENTITY_INSERT transacoes ON"
+                cursor.execute(id_on)
+
+                transacoes = f"""INSERT INTO transacoes(id, cliente_id, valor, data) 
+                            VALUES('{row[0]}',  '{row[1]}', '{row[2]}', '{row[3].replace(" -0300", "")}')"""
+                print(transacoes)
+                cursor.execute(transacoes)
+                cursor.commit()
+                print(f'id = {row[0]}, inserido com sucesso')
