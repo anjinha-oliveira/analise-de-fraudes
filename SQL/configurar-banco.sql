8aadd8b8122747ab33ee0b9f54e5e287f00238d9
Ângela Oliveira <89156781+angela-oliveir@users.noreply.github.com>
2022-08-30 20:46:00 -0300

    Fiz a analise no sql server para descobrir os dados dos clientes que tem transações fraudulentas

--- SQL/configurar-banco.sql
+++ SQL/configurar-banco.sql
@@ -29,16 +29,3 @@ select count(*) from clientes
 SET IDENTITY_INSERT transacoes ON
 SET IDENTITY_INSERT clientes ON
 
-SELECT 
-	distinct cliente_id
-FROM transacoes AS t1
-	WHERE t1.id in
-		(SELECT
-			t1.ID
-		FROM
-			transacoes t2
-		where
-			t1.cliente_id = t2.cliente_id and
-			t1.ID != t2.ID and 
-			DATEDIFF(MINUTE, t1.data, t2.data) < 2 and t2.data < t1.data
-		)
---
+++ SQL/consulta-de-clientes-fraudulentos.sql
@@ -0,0 +1,15 @@
+SELECT 
+	distinct cliente_id, clientes.nome, clientes.email, clientes.data_cadastro, clientes.telefone
+FROM transacoes AS t1
+inner join clientes
+on t1.cliente_id = clientes.ID
+	WHERE t1.id in
+		(SELECT
+			t1.ID
+		FROM
+			transacoes t2
+		where
+			t1.cliente_id = t2.cliente_id and
+			t1.ID != t2.ID and 
+			DATEDIFF(MINUTE, t1.data, t2.data) < 2 and t2.data < t1.data
+		)
