b08491db6a7413e5453344156823bebfa946092d
Ângela Oliveira <89156781+anjinha-oliveira@users.noreply.github.com>
2022-09-20 19:54:57 -0300

    Renderizando o template de "transacoes.html" utilizando Flask

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def pagina_de_clientes():
     return render_template('clientes.html')
+
+@app.route("/transacoes")
+def pagina_de_transacoes():
+    return render_template('transacoes.html')
